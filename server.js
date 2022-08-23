1111942afea3dafe96f3f22cfd3b620537bf7f59
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-08-23 10:38:00 -0500

    before tutor

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
+// const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,3 +16,13 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+// from module 
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
+
+
+
+// from challenge description******** 
+// Create the code needed in server.js to sync the Sequelize models to the MySQL database on server start. 
